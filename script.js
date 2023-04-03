d2652e5d14dd81b0a9a5470af6d29c4f69395de7
Alessandro Trupo <atr00@icloud.com>
2023-04-03 21:16:09 +0800

    Implemented a darkening rainbow mode

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const grid_container = document.querySelector('#container');
 const btn_reset = document.querySelector('#btn_reset');
 const btn_size = document.querySelector('#btn_size');
 const mode = document.querySelector('#mode_select');
+let passes_count = Array(grid_size).fill().map(() => Array(grid_size).fill(null));
 
 let mouse_down = false;
 document.body.onmousedown = () => (mouse_down = true);
@@ -18,11 +19,19 @@ function build_grid(grid_size) {
         new_item.classList.add('item');
         new_item.style['width'] = `${item_size}px`;
         new_item.style['height'] = container_size / grid_size;
+        new_item.setAttribute('data-idx', i);
         new_item.addEventListener('mouseover', function(e) {
             if (!mouse_down) return;
             set_color(e);
         });
         grid_container.appendChild(new_item);
+    }
+
+    // record the number of passes - create an empty array
+    for (let i=0; i < grid_size; i++) {
+        for (let j=0; j < grid_size; j++) {
+            passes_count[i][j] = 0;
+        }
     }
 }
 
@@ -52,9 +61,15 @@ function set_color(e) {
     if (mode.value === 'color') {
         e.target.style['background-color'] = bg_color;
     } else {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
+        // increment passes count
+        // get the index of the cell
+        const idx_i = Math.floor(e.target.dataset.idx / grid_size);
+        const idx_j = e.target.dataset.idx % grid_size;
+        
+        // set the color
+        let r = Math.max(0, Math.floor(Math.random() * 256)**(1-0.1*passes_count[idx_i][idx_j]++));
+        let g = Math.max(0, Math.floor(Math.random() * 256)**(1-0.1*passes_count[idx_i][idx_j]++));
+        let b = Math.max(0, Math.floor(Math.random() * 256)**(1-0.1*passes_count[idx_i][idx_j]++));
         e.target.style['background-color'] = `rgb(${r}, ${g}, ${b})`;
     }    
 }
